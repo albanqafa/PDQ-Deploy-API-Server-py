3f20dd0090a71f6e4edfcffbd58b3e9e769fee6f
Alban Q <37601993+albanqafa@users.noreply.github.com>
2023-07-10 22:12:59 -0400

    removing psexec

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 import re
-import subprocess
-import os
+import win32security
+import win32process
+import win32con
+import win32event
+import datetime
+
+ct = datetime.datetime.now()
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -13,14 +18,14 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 				self.end_headers()
 				return
 
-			pdq_output = self.run_command_with_different_user(package_name, computer_name)
+			command_output = self.run_command_with_different_user(package_name, computer_name)
 
 			self.send_response(200)
 			self.send_header("Content-type", "text/plain")
 			self.end_headers()
 			#self.wfile.write(pdq_output.encode())
 
-			log_entry = f"Request from: {self.client_address[0]}, Package: {package_name}, Computer: {computer_name}, PDQ Deploy output: {pdq_output}\n"
+			log_entry = f"{ct} Request from: {self.client_address[0]}, Package: {package_name}, Computer: {computer_name}, Errors: {command_output}\n"
 			try:
 				with open(self.config["logfile"], "a") as log_file:
 					log_file.write(log_entry)
@@ -40,6 +45,8 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 		if not re.match(package_regex, package_name) or package_name not in package_whitelist:
 			return False
 
+
+
 		computer_regex = r"^[a-zA-Z0-9_-]+$"
 		if not re.match(computer_regex, computer_name):
 			return False
@@ -47,30 +54,36 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 		return True
 
 	def run_command_with_different_user(self, package_name, computer_name):
-		command = [
-			"psexec.exe",
-			"-nobanner",
-			"-r", "pdq_api_backend",
-			"-u", self.config["username"],
-			"-p", self.config["password"],
-			"pdqdeploy.exe",
-			"deploy",
-			"-package", package_name,
-			"-targets", computer_name
-		]
+		command = "pdqdeploy.exe deploy -package {} -targets {}".format(package_name, computer_name)
 
-		try:
-			runas_process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			output, error = runas_process.communicate()
+		# Obtain a handle to the user's token
+		user_handle = win32security.LogonUser(
+			self.config["username"],
+			".",  # Use the local computer
+			self.config["password"],
+			win32con.LOGON32_LOGON_INTERACTIVE,  # Logon type
+			win32con.LOGON32_PROVIDER_DEFAULT  # Logon provider
+		)
 
-			if runas_process.returncode != 0:
-				return f"Error executing command: {error.decode()}"
+		# Use the token to execute the command
+		process_info = win32process.CreateProcessAsUser(
+			user_handle,  # Token
+			None,  # ApplicationName (None to use CommandLine)
+			command,  # CommandLine
+			None,  # ProcessAttributes
+			None,  # ThreadAttributes
+			0,  # bInheritHandles
+			win32con.NORMAL_PRIORITY_CLASS,  # dwCreationFlags
+			None,  # Environment (None to inherit from parent)
+			None,  # CurrentDirectory (None to inherit from parent)
+			win32process.STARTUPINFO()  # StartupInfo
+		)
 
-			return output.decode()
-		except subprocess.CalledProcessError as e:
-			return f"Error executing command: {e.output.decode()}"
-		except FileNotFoundError:
-			return "psexec.exe or pdqdeploy.exe not found"
+		# Wait for the process to complete
+		win32event.WaitForSingleObject(process_info[0], win32event.INFINITE)
+
+		return "Command executed successfully"
+
 
 def load_config(file_path):
 	config = {}
